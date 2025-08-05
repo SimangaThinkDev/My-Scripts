40a560230e15de3a06db89354111a432e43203d9
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-08-05 20:28:29 +0200

    Just returning my make django script in one piece

--- make_django.sh
+++ make_django.sh
@@ -1,32 +1,41 @@
-bash
 #!/bin/bash
 
 # Prompt user for project name
-read -rp "Enter project name: " PROJECT_NAME
+read -p "Enter project name: " PROJECT_NAME
 
 # Create Django project
 django-admin startproject $PROJECT_NAME
 
+# Immediately change into the project's directory
+cd $PROJECT_NAME
+
 # Prompt user for number of apps
-read -rp "Enter number of apps: " NUM_APPS
+while true; do
+    read -p "Enter number of apps: " NUM_APPS
+    if [[ $NUM_APPS =~ ^[0-9]+$ ]] && [ $NUM_APPS -gt 0 ]; then
+        break
+    else
+        echo "Invalid input. Please enter a positive integer."
+    fi
+done
 
 # Create apps
 for ((i=1; i<=$NUM_APPS; i++)); do
-    read -rp "Enter app name $i: " APP_NAME
+    read -p "Enter app name $i: " APP_NAME
 
     # Create app
-    python $PROJECT_NAME/manage.py startapp $APP_NAME
+    python3 manage.py startapp $APP_NAME
 
     # Create urls.py in app directory
-    touch $PROJECT_NAME/$APP_NAME/urls.py
+    touch $APP_NAME/urls.py
 
     # Edit urls.py
     echo "from django.urls import path
 app_name = '$APP_NAME'
 urlpatterns = [
     # Add your routes here
-]" > $PROJECT_NAME/$APP_NAME/urls.py
+]" > $APP_NAME/urls.py
 
     # Add app to INSTALLED_APPS
-    sed -i "/INSTALLED_APPS/a \    '$APP_NAME'," $PROJECT_NAME/$PROJECT_NAME/settings.py
+    sed -i "/INSTALLED_APPS/a \    '$APP_NAME'," $PROJECT_NAME/settings.py
 done
